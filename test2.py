62e0b42a4519838f49b3ba422613da64119ca580
SkylerHeininger <122641443+SkylerHeininger@users.noreply.github.com>
2023-04-15 14:58:31 -0400

    Including citations

--- test2.py
+++ test2.py
@@ -2,6 +2,8 @@ from gpiozero import MCP3008
 from time import sleep
 import socket
 
+# The following documentation was used to help create this script:
+# https://gpiozero.readthedocs.io/en/stable/api_spi.html#analog-to-digital-converters-adc
 
 # inp1 = MCP3008(channel=0, device=0)
 inp1 = MCP3008(channel=0, clock_pin=11, mosi_pin=10, miso_pin=9, select_pin=8)
